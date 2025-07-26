0d197e83a5933c7cf4635a36615a265849ea18e1
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-07-27 00:19:02 +0530

    + setup

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <h1 className="text-4xl font-bold text-white">Spensa Ai</h1>
+    </div>
+  );
+}

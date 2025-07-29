45e42203bec4f66096f8778153b283e67954c750
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-07-29 21:26:10 +0530

    ± theme in layout

--- src/app/(lander)/layout.tsx
+++ src/app/(lander)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/root/navbar";
+import ThemeToggleButton from "@/components/ui/theme-toggle-button";
 
 export default function LanderLayout({
   children,
@@ -9,7 +10,13 @@ export default function LanderLayout({
     <>
       <Navbar />
       <main className="flex justify-center items-center px-5 md:px-10 lg:px-20">
-        <div className="container mt-30">{children}</div>
+        <div className="container mt-30">
+          <ThemeToggleButton
+            className="fixed bottom-5 right-5 z-10 cursor-pointer"
+            start="bottom-right"
+          />
+          {children}
+        </div>
       </main>
     </>
   );

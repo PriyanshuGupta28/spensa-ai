48a798609925bf7625f7d2b6d167ca6a1bb23866
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-07-30 00:08:46 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { generateSEO } from "@/config/seo/seo.utils";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,10 +14,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Spensa Ai",
-  description: "Spensa Ai is a your personal spense assistant with AI",
-};
+export const metadata: Metadata = generateSEO();
 
 export default function RootLayout({
   children,

971dcbd58904a08c9caf41de2ae75a1373a533ee
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-07-31 03:09:38 +0530

    updated register page

--- src/app/(lander)/register/page.tsx
+++ src/app/(lander)/register/page.tsx
@@ -1,7 +1,14 @@
+import { RegisterForm } from "@/components/auth/register-form";
 import React from "react";
 
 const Register: React.FC = () => {
-  return <div>Register</div>;
+  return (
+    <div className="flex min-h-[80svh] w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <RegisterForm />
+      </div>
+    </div>
+  );
 };
 
 export default Register;

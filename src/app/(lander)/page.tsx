8d88d2a18a5c4a4375269995ee8d51b7e47d5958
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-07-31 02:59:30 +0530

    Update page.tsx

--- src/app/(lander)/page.tsx
+++ src/app/(lander)/page.tsx
@@ -1,5 +1,11 @@
+import Faq from "@/components/lander/home/faq";
 import HeroSection from "@/components/lander/home/hero-section";
 
 export default function Home() {
-  return <HeroSection />;
+  return (
+    <div className="space-y-15">
+      <HeroSection />
+      <Faq />
+    </div>
+  );
 }

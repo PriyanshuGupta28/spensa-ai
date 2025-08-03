edaf11d007cecda9766558769c7b24bf844336fd
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-08-03 05:45:11 +0530

    Create page.tsx

---
+++ src/app/(dashboard)/dashboard/income-tracker/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const IncomeTracker = () => {
+  return (
+    <div className="h-[300svh] flex justify-center items-center">page</div>
+  );
+};
+
+export default IncomeTracker;

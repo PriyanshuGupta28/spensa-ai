c2cf329b4390b6668d9584abb648fcc74aaf2483
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-08-03 23:20:18 +0530

    updated transactions page

--- src/app/(dashboard)/dashboard/transactions/page.tsx
+++ src/app/(dashboard)/dashboard/transactions/page.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { DataTable } from "./data-table";
+import { columns, Payment } from "./columns";
 
-const Transactions = () => {
-  return <div>page</div>;
+const TransactionsPage = () => {
+  const data: Payment[] = [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed5s2f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728esd52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+  ];
+  return (
+    <div className="py-5">
+      <DataTable columns={columns} data={data} key={"transactions-table"} />
+    </div>
+  );
 };
 
-export default Transactions;
+export default TransactionsPage;

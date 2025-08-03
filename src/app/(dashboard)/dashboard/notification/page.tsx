ddf33fc489e83b239ebecc808878185c48848d1e
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-08-03 05:45:07 +0530

    Create page.tsx

---
+++ src/app/(dashboard)/dashboard/notification/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Notification = () => {
+  return (
+    <div className="h-[300svh] flex justify-center items-center">page</div>
+  );
+};
+
+export default Notification;

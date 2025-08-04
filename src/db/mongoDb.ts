9134d79e2259f135ee1a952d0408a75b5f1d0b9b
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-08-05 02:46:13 +0530

    Update mongoDb.ts

--- src/db/mongoDb.ts
+++ src/db/mongoDb.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.DATABASE_URL!;
+const uri = process.env.DATABASE_URL as string;
 const client = new MongoClient(uri);
 await client.connect();
 export const db = client.db();

9a819a550cdaf6780c2450bae3a41fae36f3c16f
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-08-05 02:46:08 +0530

    Update layout.tsx

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const DashboardPage = () => {
-  return <div>Dashboard</div>;
+  const {
+    data: session,
+    isPending, //loading state
+    error, //error object
+    refetch, //refetch the session
+  } = authClient.useSession();
+
+  console.log(session);
+
+  if (!session?.user && !isPending) {
+    return redirect("/login");
+  }
+
+  return <div>{JSON.stringify(session, null, 2)}</div>;
 };
 
 export default DashboardPage;

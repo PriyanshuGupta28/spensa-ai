a5141d62538ee28c339ab29302d90d74f3fdc25d
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-08-05 23:18:22 +0530

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,12 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { db } from "@/db/mongoDb";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
   database: mongodbAdapter(db),
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [nextCookies()],
 });

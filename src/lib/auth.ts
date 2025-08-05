1d53f971cbf3b768c759dd1f2b10e2404f4cd92d
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-08-06 02:50:05 +0530

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,11 +2,19 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { db } from "@/db/mongoDb";
 import { nextCookies } from "better-auth/next-js";
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "@/utilities/contants";
 
 export const auth = betterAuth({
   database: mongodbAdapter(db),
   emailAndPassword: {
     enabled: true,
+  },
+  socialProviders: {
+    google: {
+      prompt: "select_account",
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+    },
   },
   plugins: [nextCookies()],
 });

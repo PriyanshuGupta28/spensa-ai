151adb0b17a0fb7d8d2ff4c05ca0a62586cafc3b
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-08-09 23:46:49 +0530

    Update contact-us.tsx

--- src/components/lander/contact/contact-us.tsx
+++ src/components/lander/contact/contact-us.tsx
@@ -3,13 +3,6 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import Link from "next/link";
 
 export default function ContactSection() {
